f5988232b62292a2fcdb1aa95e64e49d595ca06b
Sean Xiong <seanxionghe@gmail.com>
2020-04-25 06:23:32 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -48,10 +48,11 @@ def draw?(board)
 end
 
 
-
 def over?(board)
-  if won?(board) || full?(board) || draw?(board)
+  if draw?(board)  || won?(board) || full?(board)
     return true
+  elsif !full?(board)
+    return false
   else
     return false
   end
