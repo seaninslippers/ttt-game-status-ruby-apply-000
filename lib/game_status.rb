11f51c7e2185155ec7ea4cfce75fb220689baa9b
Sean Xiong <seanxionghe@gmail.com>
2020-04-25 01:27:37 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,7 @@ WIN_COMBINATIONS = [
   [2,4,6]]
 
 def won?(board)
-  WIN_COMBINATIONS.each do|win_combo|
+  WIN_COMBINATIONS.each {|win_combo|
     win_index_1 = win_combo[0]
     win_index_2 = win_combo[1]
     win_index_3 = win_combo[2]
@@ -29,6 +29,6 @@ def won?(board)
     elsif position_1 == "X" && position_2 == "X" && position_3 =="X"
       return win_combo
     end
-  end
+  }
     return false
 end
